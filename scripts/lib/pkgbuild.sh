b95a2803db13e1b0dd2a3cd6cf798e4f3b04c5a6
hayao <hayao@fascode.net>
2022-02-07 22:25:37 +0900

    FIx: Use customized makepkg.conf

--- scripts/lib/pkgbuild.sh
+++ scripts/lib/pkgbuild.sh
@@ -2,5 +2,5 @@
 GetPkgListFromPKGBUILD(){
     local _Arch="$1" _PkgBuild="$2"
     cd "$(dirname "$_PkgBuild")" || return 1
-    setarch "$_Arch" sudo -u "$ChrootUser" makepkg --ignorearch --packagelist -p "$(basename "${_PkgBuild}")"
+    setarch "$_Arch" sudo -u "$ChrootUser" makepkg --config "$MainDir/configs/makepkg-${_Arch}.conf" --ignorearch --packagelist -p "$(basename "${_PkgBuild}")"
 }
