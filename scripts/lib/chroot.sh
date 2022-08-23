ef90f9165619337be7ec113b059d2cac619462e1
hayao <hayao@fascode.net>
2022-08-24 00:16:05 +0900

    Fix: Setup keyring for old chroot environment

--- scripts/lib/chroot.sh
+++ scripts/lib/chroot.sh
@@ -2,7 +2,14 @@ SetupChroot_x86_64(){
     local CHROOT="$WorkDir/Chroot/x86_64/"
     MakeDir "$CHROOT"
 
-    [[ -e "$CHROOT/root/etc/pacman.conf" ]] && return 0
+    if [[ -e "$CHROOT/root/etc/pacman.conf" ]]; then
+        # Setup keyring
+        #if [[ "$(Pm.GetLatestPkgVer <<< "archlinux-keyring")" != $(Pm.GetInstalledPkgVer <<< "archlinux-keyring") ]]; then
+        #    setarch "x86_64" arch-nspawn -s "$CHROOT/root" pacman -Sy --noconfirm archlinux-keyring
+        #    setarch "x86_64" arch-nspawn -s "$CHROOT/root" pacman-key --populate archlinux
+        #fi
+        return 0
+    fi
 
     # Create chroot
     mkarchroot \
