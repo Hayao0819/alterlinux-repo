a02ebf79fdd0879da9feac13edf9089917a5c0c1
hayao <hayao@fascode.net>
2023-01-04 21:32:26 +0900

    Add: Init commit for lilac

--- repos/alter-stable/repo-config.sh
+++
@@ -1,9 +0,0 @@
-RepoArch=(
-    "x86_64"
-    "i686"
-    "pen4"
-)
-
-
-SkipPkg_x86_64=()
-SkipPkg_i686=("archlinux32-keyring")
--- start-local.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-type pacman 2> /dev/null 1>&2 || {
-    echo "You should run it on Arch Linux"
-    exit 1
-}
-
-CurrentDir="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
-bash "$CurrentDir/scripts/main.sh" "$@"
